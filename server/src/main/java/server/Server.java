2d3272397ad55102f83a6b800b790e11da8d0b65
ramazan-bagaev <ramazan.bagaev@phystech.edu>
2019-04-03 18:29:46 +0300

    removed comment

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -24,7 +24,6 @@ class Server {
             try {
                 new ClientHandler(serverSocket.accept()).start();
             } catch (IOException e) {
-                //e.printStackTrace();
                 break;
             }
         }
